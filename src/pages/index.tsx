5bae56f74f061c68ea60144214e4b718f32399c9
LAB-Cyberware <lab.cyberware@gmail.com>
2025-05-22 12:55:26 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Cualquier cosa.
           </li>
         </ol>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
